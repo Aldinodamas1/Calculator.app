42146be83d70af45ed2722807474c6b6bda89902
Aldinodamas1 <aldinosaputra718@students.amikom.ac.id>
2024-03-05 12:08:29 +0700

    menambahkan kode

---
+++ Calculator.cs
@@ -0,0 +1,15 @@
+class Calculator
+{
+	static void mian(string[] args)
+	{
+		
+	}
+	static int penambahan(int a,int b)
+	{
+		return a + b;
+	}
+	static int pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+}
